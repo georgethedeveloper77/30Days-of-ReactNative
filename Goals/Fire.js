58c24ada0c5e0051bcdcd4b8610d0645a9044349
georgethedeveloper77 <georgekarani77@gmail.com>
2020-08-19 00:41:08 +0300

    changes => goals set up

--- Goals/Fire.js
+++ Goals/Fire.js
@@ -2,80 +2,81 @@ import firebase from "firebase";
 import "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDxuuNBsAKvDDPd9kIjrIZ8X_Cxv4i0LLY",
-    authDomain: "yttodoapp.firebaseapp.com",
-    databaseURL: "https://yttodoapp.firebaseio.com",
-    projectId: "yttodoapp",
-    storageBucket: "yttodoapp.appspot.com",
-    messagingSenderId: "796524377832",
-    appId: "1:796524377832:web:4b8bf9078787f863003b5d"
+  apiKey: "AIzaSyA9wQuJFpKRUmPOxv8DeLNc5FB5CQDJqt8",
+  authDomain: "goals-8a00e.firebaseapp.com",
+  databaseURL: "https://goals-8a00e.firebaseio.com",
+  projectId: "goals-8a00e",
+  storageBucket: "goals-8a00e.appspot.com",
+  messagingSenderId: "782403996997",
+  appId: "1:782403996997:web:9526360a1a693843b9422c",
+  measurementId: "G-TP9C8LTT2P",
 };
 
 class Fire {
-    constructor(callback) {
-        this.init(callback);
+  constructor(callback) {
+    this.init(callback);
+  }
+
+  init(callback) {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
     }
 
-    init(callback) {
-        if (!firebase.apps.length) {
-            firebase.initializeApp(firebaseConfig);
-        }
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        callback(null, user);
+      } else {
+        firebase
+          .auth()
+          .signInAnonymously()
+          .catch((error) => {
+            callback(error);
+          });
+      }
+    });
+  }
 
-        firebase.auth().onAuthStateChanged(user => {
-            if (user) {
-                callback(null, user);
-            } else {
-                firebase
-                    .auth()
-                    .signInAnonymously()
-                    .catch(error => {
-                        callback(error);
-                    });
-            }
-        });
-    }
+  getLists(callback) {
+    let ref = this.ref.orderBy("name");
 
-    getLists(callback) {
-        let ref = this.ref.orderBy("name");
+    this.unsubscribe = ref.onSnapshot((snapshot) => {
+      lists = [];
 
-        this.unsubscribe = ref.onSnapshot(snapshot => {
-            lists = [];
+      snapshot.forEach((doc) => {
+        lists.push({ id: doc.id, ...doc.data() });
+      });
 
-            snapshot.forEach(doc => {
-                lists.push({ id: doc.id, ...doc.data() });
-            });
+      callback(lists);
+    });
+  }
 
-            callback(lists);
-        });
-    }
+  addList(list) {
+    let ref = this.ref;
 
-    addList(list) {
-        let ref = this.ref;
+    ref.add(list);
+  }
 
-        ref.add(list);
-    }
+  updateList(list) {
+    let ref = this.ref;
 
-    updateList(list) {
-        let ref = this.ref;
+    ref.doc(list.id).update(list);
+  }
 
-        ref.doc(list.id).update(list);
-    }
+  get userId() {
+    return firebase.auth().currentUser.uid;
+  }
 
-    get userId() {
-        return firebase.auth().currentUser.uid;
-    }
+  get ref() {
+    return firebase
+      .firestore()
+      .collection("users")
+      .doc(this.userId)
+      .collection("lists");
+  }
 
-    get ref() {
-        return firebase
-            .firestore()
-            .collection("users")
-            .doc(this.userId)
-            .collection("lists");
-    }
-
-    detach() {
-        this.unsubscribe();
-    }
+  detach() {
+    this.unsubscribe();
+  }
 }
 
 export default Fire;
